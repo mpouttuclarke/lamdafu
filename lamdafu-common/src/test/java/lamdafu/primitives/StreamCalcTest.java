3504f5bb574203c5b657f1e159aede4c7ccd2cdb
Matt Pouttu-Clarke <voidpointer101@gmail.com>
2015-12-02 11:11:24 -0700

    Added throughput

--- lamdafu-common/src/test/java/lamdafu/primitives/StreamCalcTest.java
+++ lamdafu-common/src/test/java/lamdafu/primitives/StreamCalcTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import cern.jet.random.Normal;
+import cern.jet.random.engine.MersenneTwister;
 import lamdafu.codec.Unibit;
 import lamdafu.primitives.StreamCalc;
 
@@ -11,14 +13,30 @@ public class StreamCalcTest {
 
 	@Test
 	public void test() {
-		StreamCalc target = new StreamCalc(10, 1024);
-		for(int x = 0; x < 10000000; x++) {
-			target.addOne(Math.random() * (Integer.MAX_VALUE - (x % 489392977)));
-		}
-		System.out.println(target.snapshot());
-		target = new StreamCalc(12, 1024);
+		StreamCalc target = new StreamCalc(12, 1024);
 		target.add("jimbo", "jim", "bob", "zeke");
 		System.out.println(target.snapshot());
+		target = new StreamCalc(10, 1024);
+		Normal norm = new Normal(10000, 100, new MersenneTwister(Thread.currentThread().hashCode()));
+		long start = System.currentTimeMillis();
+		int count = 20000000;
+		for(int x = 0; x < count; x++) {
+			target.addOne((double)norm.nextInt());
+		}
+		printRate(start, count, "Number only calc");
+		target.snapshot();
+		printRate(start, count, "Quantiles");
+		start = System.currentTimeMillis();
+		count = 5000000;
+		for(int x = 0; x < count; x++) {
+			target.add(String.valueOf(norm.nextInt()));
+		}
+		printRate(start, count, "Number as String calc");
+	}
+
+	private void printRate(long start, int count, String measure) {
+		double secs = (System.currentTimeMillis() - start) / 1000D;
+		System.out.println(String.format("%s count: %s, secs: %.3f, count/sec: %.0f", measure, count, secs, count / secs));
 	}
 
 }
